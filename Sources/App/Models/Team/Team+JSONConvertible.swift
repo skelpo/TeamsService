2aab8342dcf4794c097a06eab2b46a7a4e62df12
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-04 16:42:43 -0600

    Added team members to json from Team.makeJSON method

--- Sources/App/Models/Team/Team+JSONConvertible.swift
+++ Sources/App/Models/Team/Team+JSONConvertible.swift
@@ -3,7 +3,10 @@ import JSON
 extension Team: JSONConvertible {
     public func makeJSON() throws -> JSON {
         var json = JSON()
+        let children: [Member] = try self.children().all()
+        
         try json.set("name", self.name)
+        try json.set("members", children.makeJSON())
         return json
     }
     
