d9661bca2e4b692471393e9230c41ff878f6918e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-04 16:54:34 -0600

    Created GET /{INT} route

--- Sources/App/Controllers/TeamController.swift
+++ Sources/App/Controllers/TeamController.swift
@@ -9,9 +9,18 @@ public final class TeamController {
     
     public func configureRoutes() {
         builder.get(handler: all)
+        builder.get(Int.parameter, handler: getWithID)
     }
     
     public func all(_ request: Request)throws -> ResponseRepresentable {
         return try Team.all().makeJSON()
+    }
+    
+    public func getWithID(_ request: Request)throws -> ResponseRepresentable {
+        let id = try request.parameters.next(Int.self)
+        guard let team = try Team.find(id)?.makeJSON() else {
+            throw Abort(.badRequest, reason: "No team exists with the id of '\(id)'")
+        }
+        return team
     }
 }
