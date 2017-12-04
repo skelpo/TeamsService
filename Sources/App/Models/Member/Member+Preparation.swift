a4112bc125a66dd6c2fbb18ee5d986c39d845d3b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-04 15:20:10 -0600

    Conformed Member to Preparation protocol

---
+++ Sources/App/Models/Member/Member+Preparation.swift
@@ -0,0 +1,15 @@
+import Fluent
+
+extension Member: Preparation {
+    public static func prepare(_ database: Database) throws {
+        try database.create(self, closure: { (member) in
+            member.id()
+            member.int("user_id")
+            member.int("status")
+        })
+    }
+    
+    public static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
