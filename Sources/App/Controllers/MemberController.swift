fe8f3c683f2db69639e9fbdbd7f908af4f935b5f
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 07:56:33 -0600

    Created DELETE /{INT} MemberController route

--- Sources/App/Controllers/MemberController.swift
+++ Sources/App/Controllers/MemberController.swift
@@ -9,9 +9,10 @@ public final class MemberController {
     
     public func configureRoutes() {
         builder.get(Int.parameter, handler: get)
+        builder.delete(Int.parameter, handler: delete)
     }
     
-    public func get(_ request: Request)throws -> ResponseRepresentable {
+    public func memberAndTeam(from request: Request)throws -> (member: Member, team: Team) {
         let teamID = try request.parameters.next(Int.self)
         let memberID = try request.parameters.next(Int.self)
         
@@ -21,7 +22,21 @@ public final class MemberController {
         guard let member = try team.members.find(memberID) else {
             throw Abort(.badRequest, reason: "No member with the ID of '\(memberID)' exists in the specified team")
         }
+        return (member: member, team: team)
+    }
+    
+    public func get(_ request: Request)throws -> ResponseRepresentable {
+        return try memberAndTeam(from: request).member.makeJSON()
+    }
+    
+    public func delete(_ request: Request)throws -> ResponseRepresentable {
+        guard let status = request.data["status"]?.int,
+            MemberStatus(rawValue: status) == .admin else {
+                throw Abort(.forbidden, reason: "User doers not have required privileges")
+        }
+        let member = try memberAndTeam(from: request).member
+        try member.delete()
         
-        return try member.makeJSON()
+        return Response(status: .ok)
     }
 }
