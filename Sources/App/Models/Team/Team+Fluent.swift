8d099651cd39ccc2801e183abfdceff185fdff13
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 08:54:45 -0600

    Created Team.add([Int]) extension method

--- Sources/App/Models/Team/Team+Fluent.swift
+++ Sources/App/Models/Team/Team+Fluent.swift
@@ -4,4 +4,10 @@ extension Team {
     public var members: Siblings<Team, Member, Pivot<Team, Member>> {
         return siblings()
     }
+    
+    public func add(members: [Int])throws {
+        try Member.makeQuery().filter("id", in: members).all().forEach({ member in
+            try self.members.add(member)
+        })
+    }
 }
