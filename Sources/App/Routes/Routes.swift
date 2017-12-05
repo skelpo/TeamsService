c81cc6e0339212798d71687fe88c5c4542c47e86
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 12:13:57 -0600

    Added PayloadAuthenticationMiddleware to all routes

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,8 +1,17 @@
 import Vapor
+import JWTProvider
+import JWT
 
 extension Droplet {
     func setupRoutes() throws {
-        let api = self.grouped(APIErrorMiddleware())
+        guard let jwkUrl: String = self.config["app", "jwks_url"]?.string else {
+            fatalError("Missing 'jwks_url' value in app config")
+        }
+        let expirationClaim = ExpirationTimeClaim()
+        let jwtMiddleware = PayloadAuthenticationMiddleware<Member>(jwkUrl, [expirationClaim], Member.self)
+        
+        let api = self.grouped(APIErrorMiddleware(), jwtMiddleware)
+        
         TeamController(builder: api).configureRoutes()
         MemberController(builder: api).configureRoutes()
     }
