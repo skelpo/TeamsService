01039c6732216476a7c57c5243565cb1bb163808
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 07:27:05 -0600

    Created GET /{INT} MemberController route

--- Sources/App/Controllers/MemberController.swift
+++ Sources/App/Controllers/MemberController.swift
@@ -8,6 +8,20 @@ public final class MemberController {
     }
     
     public func configureRoutes() {
+        builder.get(Int.parameter, handler: get)
+    }
+    
+    public func get(_ request: Request)throws -> ResponseRepresentable {
+        let teamID = try request.parameters.next(Int.self)
+        let memberID = try request.parameters.next(Int.self)
         
+        guard let team = try Team.find(teamID) else {
+            throw Abort(.badRequest, reason: "No team exists with the ID of '\(teamID)'")
+        }
+        guard let member = try team.members.find(memberID) else {
+            throw Abort(.badRequest, reason: "No member with the ID of '\(memberID)' exists in the specified team")
+        }
+        
+        return try member.makeJSON()
     }
 }
