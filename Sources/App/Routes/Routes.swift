e98343b8e02ea6458190cb142c7b1a0d681bc409
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 10:33:08 -0600

    Added MemberController config to Droplet.setupRoutes with api group

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,6 +2,8 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        TeamController(drop: self).configureRoutes()
+        let api = self.grouped(APIErrorMiddleware())
+        TeamController(builder: api).configureRoutes()
+        MemberController(builder: api).configureRoutes()
     }
 }
