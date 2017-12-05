62ff536ed8262a1c4959498b9bc3d62ca0f554cd
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-05 08:23:33 -0600

    Created DELETE /{INT} TeamController route

--- Sources/App/Controllers/TeamController.swift
+++ Sources/App/Controllers/TeamController.swift
@@ -3,6 +3,8 @@ import Vapor
 public final class TeamController {
     let builder: RouteBuilder
     
+    // MARK: - Configuration
+    
     public init(drop: Droplet) {
         self.builder = drop.grouped("teams").grouped(APIErrorMiddleware())
     }
@@ -10,9 +12,12 @@ public final class TeamController {
     public func configureRoutes() {
         builder.get(handler: all)
         builder.get(Int.parameter, handler: getWithID)
+        builder.delete(Int.parameter, handler: delete)
         
         MemberController(builder: builder).configureRoutes()
     }
+    
+    // MARK: - Route
     
     public func all(_ request: Request)throws -> ResponseRepresentable {
         return try Team.all().makeJSON()
@@ -24,5 +29,19 @@ public final class TeamController {
             throw Abort(.badRequest, reason: "No team exists with the id of '\(id)'")
         }
         return team
+    }
+    
+    public func delete(_ request: Request)throws -> ResponseRepresentable {
+        guard let status = request.data["status"]?.int,
+            MemberStatus(rawValue: status) == .admin else {
+                throw Abort(.forbidden, reason: "User doers not have required privileges")
+        }
+        let teamID = try request.parameters.next(Int.self)
+        guard let team = try Team.find(teamID) else {
+            throw Abort(.badRequest, reason: "No team exists with the ID of '\(teamID)'")
+        }
+        try team.delete()
+        
+        return Response(status: .ok)
     }
 }
