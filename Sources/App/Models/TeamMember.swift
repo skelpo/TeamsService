ff8c62d2fd2dc184a48349c67ca16d7f2cfa5a4c
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 15:28:28 -0600

    Created TeamMmber model class

---
+++ Sources/App/Models/TeamMember.swift
@@ -0,0 +1,15 @@
+import FluentProvider
+
+public final class TeamMember {
+    public let storage: Storage = Storage()
+    
+    public let userID: Int
+    public let teamID: Int
+    public let status: Int
+    
+    public init(userID: Int, teamID: Int, status: Int) {
+        self.userID = userID
+        self.teamID = teamID
+        self.status = status
+    }
+}
