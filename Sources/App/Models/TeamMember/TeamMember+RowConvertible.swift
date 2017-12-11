a1240dca487a48c46b62568f9e169fac05770576
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 15:38:38 -0600

    Conformed TeamMember to RowConvertible protocol

---
+++ Sources/App/Models/TeamMember/TeamMember+RowConvertible.swift
@@ -0,0 +1,21 @@
+import Fluent
+
+extension TeamMember: RowConvertible {
+    public func makeRow() throws -> Row {
+        var row = Row()
+        
+        try row.set("user_id", self.userID)
+        try row.set("team_id", self.teamID)
+        try row.set("status", self.status)
+        
+        return row
+    }
+    
+    public convenience init(row: Row) throws {
+        let userID: Int = try row.get("user_id")
+        let teamID: Int = try row.get("team_id")
+        let status: Int = try row.get("status")
+        
+        self.init(userID: userID, teamID: teamID, status: status)
+    }
+}
