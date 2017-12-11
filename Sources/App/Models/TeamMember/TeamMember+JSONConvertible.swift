ca53c2fe5d8f58fe459f1d0c65d95e518158a894
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 15:44:44 -0600

    Conformed TeamMember to JSONConvertible protocol

---
+++ Sources/App/Models/TeamMember/TeamMember+JSONConvertible.swift
@@ -0,0 +1,25 @@
+import JSON
+
+extension TeamMember: JSONConvertible {
+    public func makeJSON() throws -> JSON {
+        var json = JSON()
+        guard let statusName = MemberStatus(rawValue: self.status) else {
+            throw MemberError.undefinedMemberStatus(self.status)
+        }
+        
+        try json.set("id", self.id?.wrapped)
+        try json.set("user_id", self.userID)
+        try json.set("status", self.status)
+        try json.set("status_name", statusName)
+        
+        return json
+    }
+    
+    public convenience init(json: JSON) throws {
+        let userID: Int = try json.get("user_id")
+        let teamID: Int = try json.get("team_id")
+        let status: Int = try json.get("status")
+        
+        self.init(userID: userID, teamID: teamID, status: status)
+    }
+}
