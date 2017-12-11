86141e24e615ef995954286d0931c6dedeeea578
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 15:58:16 -0600

    Removed Team.add method

--- Sources/App/Models/Team/Team+Fluent.swift
+++ Sources/App/Models/Team/Team+Fluent.swift
@@ -4,10 +4,4 @@ extension Team {
     public func members()throws -> Query<TeamMember> {
         return try TeamMember.makeQuery().filter("team_id", self.id)
     }
-    
-    public func add(members: [Int])throws {
-        try Member.makeQuery().filter("id", in: members).all().forEach({ member in
-            try self.members.add(member)
-        })
-    }
 }
