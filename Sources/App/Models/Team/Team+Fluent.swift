a904bcd6641a5c329caef14882eb3eb930adb58e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 09:42:24 -0600

    Documented Team+Fluent.swift

--- Sources/App/Models/Team/Team+Fluent.swift
+++ Sources/App/Models/Team/Team+Fluent.swift
@@ -1,6 +1,9 @@
 import Fluent
 
+/// `Team` model heplers for interacting with the `teams` database table.
 extension Team {
+    
+    /// Creates a query that gets all the members that belong to the team.
     public func members()throws -> Query<TeamMember> {
         return try TeamMember.makeQuery().filter("teamId", self.id)
     }
