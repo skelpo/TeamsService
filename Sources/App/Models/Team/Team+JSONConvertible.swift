7ccda0fd2ee43bd55418af4644346865730f5e2e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 09:37:50 -0600

    Documented Team+JSONConvertible.swift

--- Sources/App/Models/Team/Team+JSONConvertible.swift
+++ Sources/App/Models/Team/Team+JSONConvertible.swift
@@ -1,18 +1,29 @@
 import JSON
 
+/// Conformes the `Team` model to `JSONConvertible`.
 extension Team: JSONConvertible {
+    
+    /// Creates a JSON representation of the model instance.
     public func makeJSON() throws -> JSON {
+        // Create the JSON instance and get all the team's members
         var json = JSON()
         let members: [TeamMember] = try self.members().all()
         
+        // Set the `id`, `name`, and `members` JSON keys
         try json.set("id", self.id?.wrapped)
         try json.set("name", self.name)
         try json.set("members", members.makeJSON())
+        
+        // Return the JSON
         return json
     }
     
+    /// Creates an instance of `Team` from a JSON object.
     public convenience init(json: JSON) throws {
+        // Get the team's name from the JSON
         let name: String = try json.get("name")
+        
+        // Create the team.
         self.init(name: name)
     }
 }
