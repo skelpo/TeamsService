95ba82155c78f281cedf99881d1dc8c5ccd2a9e3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 10:29:10 -0600

    Documented TeamMember+Fluent.swift

--- Sources/App/Models/TeamMember/TeamMember+Fluent.swift
+++ Sources/App/Models/TeamMember/TeamMember+Fluent.swift
@@ -1,8 +1,15 @@
 import Fluent
 
+/// `TeamMember` model heplers for interacting with the `team_memberss` database table.
 extension TeamMember {
+    
+    /// Create a query that gets all the teams that the member is a part of.
     public func teams()throws -> Query<Team> {
+        
+        // Get all the IDs of the teams the user is part of.
         let ids = try TeamMember.makeQuery().filter("userId", self.userID).all().map({ $0.teamID })
+        
+        // Get all the teams that have an ID in the `ids` array.
         return try Team.makeQuery().filter("id", in: ids)
     }
 }
