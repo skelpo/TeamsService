4c419203ce33661eb6afcaa16b6013887c09d378
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 09:18:53 -0600

    Documented Team+RowRepresentable.swift

--- Sources/App/Models/Team/Team+RowRepresentable.swift
+++ Sources/App/Models/Team/Team+RowRepresentable.swift
@@ -1,7 +1,13 @@
 import Fluent
 
+/// Conformes the `Team` model to `RowRepresentable`.
 extension Team: RowRepresentable {
+    
+    /// Creates a `Row` from the model so Fluent can store the model in the database.
+    /// Very rarely, if ever, will you have to call this method directly.
     public func makeRow() throws -> Row {
+        
+        // Create a row, set the name property, and return it.
         var row = Row()
         try row.set("name", self.name)
         return row
