41e11478afd6f943a2aeedf19184f1f905b58a43
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 09:12:36 -0600

    Documented TeamError.swift

--- Sources/App/Models/Team/TeamError.swift
+++ Sources/App/Models/Team/TeamError.swift
@@ -1,6 +1,11 @@
+/// Errors that occur when interacting with the `Team` model.
 public enum TeamError: Error, CustomStringConvertible {
+    
+    /// Thrown when no team is found with an ID that a user is a member of.
     case noTeamForID(Int)
     
+    /// API friendly versions of the error.
+    /// `APIErrorMiddleware` uses this property to generate the error JSON.
     public var description: String {
         switch self {
         case let .noTeamForID(id): return "No team was found with the ID '\(id)'"
