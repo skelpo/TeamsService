5e6c30159e2aa3cfdb3bfd1b68abe6902066a370
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 09:09:47 -0600

    Conformed TeamError to CustomStringConvertible protocol

--- Sources/App/Models/Team/TeamError.swift
+++ Sources/App/Models/Team/TeamError.swift
@@ -1,3 +1,9 @@
-public enum TeamError: Error {
+public enum TeamError: Error, CustomStringConvertible {
     case noTeamForID(Int)
+    
+    public var description: String {
+        switch self {
+        case let .noTeamForID(id): return "No team was found with the ID '\(id)'"
+        }
+    }
 }
