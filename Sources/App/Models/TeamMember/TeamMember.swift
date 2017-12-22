28cf10f98a8bd5e863bfee1d3135e4fa43e33da3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 10:00:37 -0600

    Documented TeamMember.swift

--- Sources/App/Models/TeamMember/TeamMember.swift
+++ Sources/App/Models/TeamMember/TeamMember.swift
@@ -1,18 +1,45 @@
 import FluentProvider
 
+/// Represents a member for a team.
 public final class TeamMember: Model {
+    
+    /// Used by Fluent to store metadata for the model in the database.
     public let storage: Storage = Storage()
     
+    
+    /// The ID of the user that model represents.
     public let userID: Int
+    
+    /// The ID of the team that the user is a member of.
     public let teamID: Int
+    
+    /// The status the member holds in the team ('admin' or 'standard').
     public let status: Int
     
+    
+    /// Creates a `TeamMember` with the neccesary information.
+    ///
+    /// - Parameters:
+    ///  - userID: The ID of the user that will be a member of a team.
+    ///  - teamID: The ID of the team the user will be a member of.
+    ///  - status: The status the member will hold in the team.
+    ///
+    /// - Note: This initializer will not save the member to the database.
     public init(userID: Int, teamID: Int, status: MemberStatus) {
         self.userID = userID
         self.teamID = teamID
         self.status = status.rawValue
     }
     
+    /// Creates a `TeamMember` with the neccesary information.
+    ///
+    /// - Parameters:
+    ///  - userID: The ID of the user that will be a member of a team.
+    ///  - teamID: The ID of the team the user will be a member of.
+    ///  - status: The raw value of the status the member will hold in the team.
+    /// - Throws: `MemberError.undefinedMemberStatus` if a bad status value is passed in.
+    ///
+    /// - Note: This initializer will not save the member to the database.
     public convenience init(userID: Int, teamID: Int, status: Int)throws {
         guard let memberStatus = MemberStatus(rawValue: status) else {
             throw MemberError.undefinedMemberStatus(status)
