970edc959737e620783e205a61a17cceee9a8714
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 14:23:16 -0600

    Added id property to Team model

--- Sources/App/Models/Team/Team.swift
+++ Sources/App/Models/Team/Team.swift
@@ -7,9 +7,15 @@ final class Team: Content {
     /// The name of the team.
     let name: String
     
+    /// The ID row that holds the representation of the model on the database
+    /// This property is a varible because Fluent has to be able to mutate it
+    /// (from `nil` to `n`).
+    var id: Int?
+    
     /// Creates a new team with a given name.
     /// - Note: The team is _not_ save to the database with this method.
     init(name: String) {
         self.name = name
     }
 }
+
