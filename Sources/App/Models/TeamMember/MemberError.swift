11f16a99be22c32991fe5467d019f6af00d5b764
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 15:14:46 -0600

    Created MemberError.notSaved case

--- Sources/App/Models/TeamMember/MemberError.swift
+++ Sources/App/Models/TeamMember/MemberError.swift
@@ -4,11 +4,16 @@ enum MemberError: Error, CustomStringConvertible {
     /// Thrown when an attempt is made to create a user with a status that does not exist.
     case undefinedMemberStatus(Int)
     
+    /// Thrown when you attempt to access the ID of a `TeamMember` model
+    /// that was not saved to the database
+    case notSaved
+    
     /// An API friendly versions of the error.
     /// `APIErrorMiddleware` uses this property to generate the error JSON.
     var description: String {
         switch self {
         case let .undefinedMemberStatus(status): return "No member status exists with the int value '\(status)'"
+        case .notSaved: return "The member you attempted to access is not saved to the database"
         }
     }
 }
