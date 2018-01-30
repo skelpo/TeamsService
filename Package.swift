68668b656f77669c56c2b2f8c0bd226d65013561
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 13:52:24 -0600

    Updated packages for Vapor 3

--- Package.swift
+++ Package.swift
@@ -9,21 +9,20 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/vapor/mysql-provider.git", .exact("2.0.0")),
-        .package(url: "https://github.com/vapor/jwt-provider.git", .exact("1.3.0")),
-        .package(url: "https://github.com/Skelpo/SkelpoMiddleware.git", .exact("1.3.1"))
+        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/mysql-driver.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/jwt.git", .branch("beta")),
+        .package(url: "https://github.com/Skelpo/SkelpoMiddleware.git", .branch("vapor-3"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "MySQLProvider", "JWTProvider", "SkelpoMiddleware"],
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JWT", "SkelpoMiddleware"],
                 exclude: [
                     "Config",
                     "Public",
                     "Resources",
                 ]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
 
