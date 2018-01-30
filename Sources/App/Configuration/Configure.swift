36e7a73925e2128fbbc716e31416153b4ab0ee19
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-30 14:00:16 -0600

    Created global configure method

---
+++ Sources/App/Configuration/Configure.swift
@@ -0,0 +1,20 @@
+import Vapor
+import FluentMySQL
+
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+    ) throws {
+    try services.register(FluentProvider())
+    try services.register(FluentMySQLProvider())
+    
+    var dbConfig = DatabaseConfig()
+    let database = MySQLDatabase(hostname: "localhost", user: "root", password: nil, database: "service_teams")
+    dbConfig.add(database: database, as: .mysql)
+    services.register(dbConfig)
+    
+    var migirateConfig = MigrationConfig()
+    migirateConfig.add(model: Package.self, database: .mysql)
+    services.register(migirateConfig)
+}
