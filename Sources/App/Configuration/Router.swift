049e674da432b7f73e3197a9f4f103121b92f601
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-08 08:10:52 -0600

    Documented Routes class

--- Sources/App/Configuration/Router.swift
+++ Sources/App/Configuration/Router.swift
@@ -1,13 +1,19 @@
 import Routing
 import Vapor
 
+/// The main route collection for the application.
+/// It is used to register the app's routes with the router.
 final class Routes: RouteCollection {
+    
+    /// The application that the routes are added to.
     let app: Application
 
+    /// Creates a new `Routes` collection and injects the app that created the router.
     init(app: Application) {
         self.app = app
     }
     
+    /// Adds the controllers' routes to the router.
     func boot(router: Router) throws {
         try router.register(collection: MemberController())
     }
