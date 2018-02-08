6e4d49900363e4b62fd4b10cb77965ca70abf3b5
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-08 07:33:43 -0600

    Documented global boot function

--- Sources/App/Configuration/Boot.swift
+++ Sources/App/Configuration/Boot.swift
@@ -1,9 +1,17 @@
 import Routing
 import Vapor
 
+/// Not sure why they called this function `boot`.
+/// This function actually registers the routes with
+/// the application's router.
 public func boot(_ app: Application) throws {
     
+    // Get the router service held by the app.
     let router = try app.make(Router.self)
+    
+    // Initialize the app's route register.
     let routes = Routes(app: app)
+    
+    // Register the routes with the application's router.
     try router.register(collection: routes)
 }
