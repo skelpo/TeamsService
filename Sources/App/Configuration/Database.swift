1332f9a2dfbc52d239ccecad36c69b7e5daf8db8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-08 08:05:55 -0600

    Documented mysql DatabaseIdentifier property

--- Sources/App/Configuration/Database.swift
+++ Sources/App/Configuration/Database.swift
@@ -2,6 +2,8 @@ import FluentMySQL
 import Vapor
 
 extension DatabaseIdentifier {
+    
+    /// Not really sure what this is for, but you need it.
     static var mysql: DatabaseIdentifier<MySQLDatabase> {
         return .init("mysql")
     }
