b4c68a949ee163b890e6b14a425803c80d218a21
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-08 08:04:50 -0600

    Documented configure function

--- Sources/App/Configuration/Configure.swift
+++ Sources/App/Configuration/Configure.swift
@@ -1,19 +1,27 @@
 import Vapor
 import FluentMySQL
 
+/// Configures the services that will be used in the application.
 public func configure(
     _ config: inout Config,
     _ env: inout Environment,
     _ services: inout Services
-    ) throws {
+) throws {
+    
+    // Register the Fluent and FluentMySQL providers.
+    // This configures migrations and the database connection.
     try services.register(FluentProvider())
     try services.register(FluentMySQLProvider())
     
+    // Create the database config.
     var dbConfig = DatabaseConfig()
+    
+    // Setup the connection to the database.
     let database = MySQLDatabase(hostname: "localhost", user: "root", password: nil, database: "service_teams")
     dbConfig.add(database: database, as: .mysql)
     services.register(dbConfig)
     
+    // Add the models to the migration config so tables are create for them.
     var migirateConfig = MigrationConfig()
     migirateConfig.add(model: Team.self, database: .mysql)
     migirateConfig.add(model: TeamMember.self, database: .mysql)
