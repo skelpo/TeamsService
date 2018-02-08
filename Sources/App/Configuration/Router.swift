c2d176896f04ff72e9582e46eaca84175b2c65d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-08 08:12:40 -0600

    Moved Routes.swift routing login to Routes.boot method

--- Sources/App/Configuration/Router.swift
+++ Sources/App/Configuration/Router.swift
@@ -1,5 +1,6 @@
 import Routing
 import Vapor
+import SkelpoMiddleware
 
 /// The main route collection for the application.
 /// It is used to register the app's routes with the router.
@@ -15,6 +16,27 @@ final class Routes: RouteCollection {
     
     /// Adds the controllers' routes to the router.
     func boot(router: Router) throws {
-        try router.register(collection: MemberController())
+        // TODO: - Add `JWTAuthenticationMiddleware` to api route group.
+        
+        // Create a route group with middleware for:
+        // 1. Converting errors to JSON
+        // 2. Authenticating a user with a JWT token
+        // 3. Getting the team IDs from the JWT payload
+        let api = router.grouped(
+            APIErrorMiddleware(),
+            TeamIDMiddleware()
+        ).grouped("*", "teams")
+        
+        // Configure the routes in the `TeamController` and `MemberController` with the `api` route group.
+        // This method requires the controllers to conform to the `RouteCollection` and `EmptyInitializable` protocols.
+        try api.register(collection: TeamController())
+        try api.register(collection: MemberController())
+        
+        // Create a route at the path `/teams/health` using a closure for the handler.
+        // This route is used by the AWS E2C instance to check the health of the server.
+        // We are registering the route here because we can't have it behind an authentication layer.
+        router.get("*", "teams", "health") { request in
+            return "all good"
+        }
     }
 }
