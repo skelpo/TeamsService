e726ef9399dd869db214e50590fc8c2534fbc0d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-27 11:01:52 -0500

    Installed JSON package

--- Package.swift
+++ Package.swift
@@ -13,10 +13,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/Skelpo/SkelpoMiddleware", from: "1.4.0-rc.6.2"),
-        .package(url: "https://github.com/Skelpo/JWTVapor", from: "0.7.1")
+        .package(url: "https://github.com/Skelpo/JWTVapor", from: "0.7.1"),
+        .package(url: "https://github.com/skelpo/JSON.git", from: "0.12.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JWT", "SkelpoMiddleware", "JWTVapor"],
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JWT", "SkelpoMiddleware", "JWTVapor", "JSON"],
                 exclude: [
                     "Config",
                     "Public",
