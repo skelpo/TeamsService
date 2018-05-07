809a17627a5ab19fe359889ab94d84afe2c41ce4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-07 16:55:45 -0500

    Replaced JWTVerificationMiddleware with JWTStorageMiddleware

--- Sources/App/Configuration/Router.swift
+++ Sources/App/Configuration/Router.swift
@@ -11,7 +11,7 @@ public func routes(_ router: Router)throws {
     // 3. Getting the team IDs from the JWT payload
     let api = router.grouped(
         TeamIDMiddleware<Payload>(),
-        JWTVerificationMiddleware<Payload>()
+        JWTStorageMiddleware<Payload>()
     ).grouped(any, "teams")
     
     // Configure the routes in the `TeamController` and `MemberController` with the `api` route group.
